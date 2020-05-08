be1a77f0fa28c4e4a20d4d75b478a2b2ecb5f181
istuffer-malpack <60739047+istuffer-malpack@users.noreply.github.com>
2020-05-08 08:14:32 -0400

    bug fix

--- js/finleyApp.js
+++ js/finleyApp.js
@@ -260,6 +260,8 @@ app.directive('autoComplete', function($timeout) {
 					$scope.productId = prodid;
 					var modal = document.getElementById('skidTagPrint');
 					$('.bc-form input').val('');
+					$('#printLabelBarcode .bc-form p.nine.columns').html('Enter multiple/skipping Barcode IDs with a dash <b class="red-text">(-)</b> and/or semicolon <b class="red-text">(;)</b>. <span class="stacked"><b style="color:teal;font-weight: bold;">0000001-0000008;0000011;0000022-0000025</b></span>');
+					$('#skidTagPrint .bc-form p.nine.columns').html('Enter multiple/skipping Barcode IDs with a dash <b class="red-text">(-)</b> and/or semicolon <b class="red-text">(;)</b>. <span class="stacked"><b style="color:teal;font-weight: bold;">0000001-0000008;0000011;0000022-0000025</b></span>');
 					modal.style.display = "block";					
 				}
 			
